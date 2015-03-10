3004578cbca86a250fb317c36072c916c21a9472
Mischa Gorinskat <m.gorinskat@gmail.com>
2015-03-10 13:35:52 +0100

    Renames Path to topic service for 4.4

--- Services/SolrQueryService.php
+++ Services/SolrQueryService.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Translation\Translator;
-use Newscoop\Topic\TopicService;
+use Newscoop\NewscoopBundle\Services\TopicService;
 use Newscoop\SolrSearchPluginBundle\Exception\SolrException;
 use Newscoop\SolrSearchPluginBundle\Search\SolrQuery;
 use Newscoop\SolrSearchPluginBundle\Services\SolrHelperService;
@@ -55,7 +55,7 @@ class SolrQueryService
     private $em;
 
     /**
-     * @var Newscoop\Topic\TopicService
+     * @var Newscoop\NewscoopBundle\Services\TopicService
      */
     private $topicService;
 
